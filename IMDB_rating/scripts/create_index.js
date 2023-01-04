842f6b2bf773fd2a5871905c996fac6eeeeb1032
Sudo17 <57996870+Sudo17@users.noreply.github.com>
2023-01-04 20:04:05 +0000

    Delete IMDB_rating directory

--- IMDB_rating/scripts/create_index.js
+++
@@ -1,7 +0,0 @@
-conn = new Mongo();
-db = conn.getDB("imdb");
-db.movies.ensureIndex( {"movie_id":1} )
-db.movies.ensureIndex( {"rating":1} )
-db.movies.ensureIndex( {"genre":1} )
-db.movies.ensureIndex( {"produced":1} )
-db.movies.ensureIndex( {"name":1} )
